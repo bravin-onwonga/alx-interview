112122123be5ab117d383a22f7ae581dcae345cc
bravin-onwonga <onbravin@gmail.com>
2024-08-01 17:37:41 +0300

    First try

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Lockboxes puzzles"""
+
+def canUnlockAll(boxes):
+    """Checks if all boxes can be unclocked"""
+    opened = [0]
+
+    res = open_boxes(boxes, boxes[0], opened)
+
+    return res
+
+
+def open_boxes(boxes, keys, opened):
+    """Opens boxes"""
+    if len(opened) == len(boxes):
+        return True
+
+    if len(keys) == 0:
+        return False
+
+    for key in keys:
+        if key not in opened and key < len(boxes):
+            opened.append(key)
+            return open_boxes(boxes, boxes[key], opened)
+    return True
