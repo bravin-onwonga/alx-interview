651557a1906af53ad5755308043516915e3e8b02
bravin-onwonga <onbravin@gmail.com>
2024-08-16 15:58:16 +0300

    Create copy if length is half or less of n and paste

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,36 +1,50 @@
-#!/usr/bin/python3
-"""Calculates the minimum number of operations to achieve n characters in a string"""
+  #!/usr/bin/python3
+"""Calculates the minimum number of operations
+to achieve n characters in a string"""
 
 
 def minOperations(n):
     """Function that returns the minimum number of operations to
     achieve n characters"""
     my_str = "H"
+
     count = 0
 
     if n <= 1:
         return 1
-    while (len(my_str) < n):
-        cpy = copyAll(my_str)
-
-        if (len(my_str) / 2 < n):
+    try:
+        while (len(my_str) < n):
             cpy = copyAll(my_str)
+            count += 1
             my_str = paste(my_str, cpy)
             count += 1
-        else:
+            if (len(my_str) >= n):
+                break
+            if ((n / 2) >= (len(my_str))):
+                cpy = copyAll(my_str)
+                count += 1
             my_str = paste(my_str, cpy)
             count += 1
-    if (len(my_str) >= n):
+            if (len(my_str) >= n):
+                break
         return count
-    return (0)
+    except BaseException:
+        return 0
+
 
 def copyAll(str):
     """Creates a copy of str"""
     str_cpy = ""
     for c in str:
-        str_cpy.join(c)
+        str_cpy += c
     return str_cpy
+
 
 def paste(str1, str2):
     """Concatenates two strings"""
-    return str1 + str2
+    new_str = ""
+    for c in str1:
+        new_str += c
+    for c in str2:
+        new_str += c
+    return new_str
