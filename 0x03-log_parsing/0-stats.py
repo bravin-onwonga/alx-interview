77fbb8d407b0d229cf8c23d64b672cea86d56de0
bravin-onwonga <onbravin@gmail.com>
2024-08-16 12:41:16 +0300

    Using try/except block inplace of signal.SIGINT

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -4,13 +4,13 @@ Reads stdin line by line and computes metrics
 """
 
 import sys
-import signal
+
+total_size = 0
+count = 0
+status_codes = {}
+
 
 if __name__ == '__main__':
-    total_size = 0
-    count = 0
-    status_codes = {}
-
     def print_logs(status_codes: dict, total_size: int) -> None:
         """Handles printing after 10 lines are read and/or after
         CTRL-C is pressed with status codes sorted in ascending order
@@ -25,24 +25,26 @@ if __name__ == '__main__':
         sorted_dict = {i: status_codes[i] for i in codes}
         for code, num in sorted_dict.items():
             print(f'{code}: {num}')
-
-    for line in sys.stdin:
-        """Loops through stdinput until CTRL-C is pressed
-        Returns nothing
-        """
-        n = 10
-        my_lst = line.split(" ")
-        status_code = my_lst[-2]
-        if status_codes.get(status_code):
-            status_codes[status_code] += 1
-        else:
-            status_codes[status_code] = 1
-        file_size = int(my_lst[-1])
-        total_size += file_size
-        count += 1
-        if count == 10:
-            print_logs(status_codes, total_size)
-            count = 0
-            status_codes = {}
-
-    signal.signal(signal.SIGINT)
+    try:
+        for line in sys.stdin:
+            """Loops through stdinput until CTRL-C is pressed
+            Returns nothing
+            """
+            n = 10
+            my_lst = line.split(" ")
+            status_code = my_lst[-2]
+            if status_codes.get(status_code):
+                status_codes[status_code] += 1
+            else:
+                status_codes[status_code] = 1
+            file_size = int(my_lst[-1])
+            total_size += file_size
+            count += 1
+            if count == 10:
+                print_logs(status_codes, total_size)
+                count = 0
+    except KeyboardInterrupt:
+        print_logs(status_codes, total_size)
+        raise
+    except Exception as e:
+        pass
