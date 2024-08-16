becd3d93cfa1eb23809e473212c807e3a9e17a0b
bravin-onwonga <onbravin@gmail.com>
2024-08-16 15:19:46 +0300

    Checking if there is need to cpy string

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,50 +1,36 @@
 #!/usr/bin/python3
-"""Calculates the minimum number of operations
-to achieve n characters in a string"""
+"""Calculates the minimum number of operations to achieve n characters in a string"""
 
 
 def minOperations(n):
     """Function that returns the minimum number of operations to
     achieve n characters"""
     my_str = "H"
-
     count = 0
 
     if n <= 1:
         return 1
-    try:
-        while (len(my_str) < n):
-            cpy = copyAll(my_str)
-            count += 1
-            my_str = paste(my_str, cpy)
-            count += 1
-            if (len(my_str) >= n):
-                break
-            my_str = paste(my_str, cpy)
-            count += 1
-            if (len(my_str) >= n):
-                break
-            if (len(my_str) + len(cpy) >= n):
-                my_str = paste(my_str, cpy)
-                count += 1
-        return count
-    except BaseException:
-        return 0
+    while (len(my_str) < n):
+        cpy = copyAll(my_str)
 
+        if (len(my_str) / 2 < n):
+            cpy = copyAll(my_str)
+            my_str = paste(my_str, cpy)
+            count += 1
+        else:
+            my_str = paste(my_str, cpy)
+            count += 1
+    if (len(my_str) >= n):
+        return count
+    return (0)
 
 def copyAll(str):
     """Creates a copy of str"""
     str_cpy = ""
     for c in str:
-        str_cpy += c
+        str_cpy.join(c)
     return str_cpy
-
 
 def paste(str1, str2):
     """Concatenates two strings"""
-    new_str = ""
-    for c in str1:
-        new_str += c
-    for c in str2:
-        new_str += c
-    return new_str
+    return str1 + str2
