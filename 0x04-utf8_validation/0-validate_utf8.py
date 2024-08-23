9bcb66bc2f3cc7f89e38797259efbc946d3f1e66
bravin-onwonga <onbravin@gmail.com>
2024-08-23 16:29:34 +0300

    Checking if number is larger than 4 digits

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -9,15 +9,38 @@ def validUTF8(data):
         True - if is valid
         False - not valid
     """
+    bin_lst = []
 
     for num in data:
-        bin = '{0:08b}'.format(num)
-        if bin[0] == '0':
-            continue
-        else:
-            idx = 0
-            while bin[idx] == '1':
-                idx += 1
-            if idx > 4:
+        bin_lst.append('{0:08b}'.format(num))
+
+    len_bin_lst = len(bin_lst)
+
+    if (bin_lst[0][0] == '0'):
+        more_than_one = False
+    else:
+        more_than_one = True
+        idx = 0
+        bin = bin_lst[0]
+        while bin[idx] == '1':
+            idx += 1
+        if idx > 4:
+            return False
+        if idx != len_bin_lst:
+            return False
+
+    idx = 0
+    if more_than_one:
+        idx = 1
+
+    while idx < len_bin_lst:
+        bin = bin_lst[idx]
+
+        if more_than_one:
+            if bin[0] != '1' and bin[2] != 0:
                 return False
+        else:
+            if bin[0] == '1':
+                return False
+        idx += 1
     return True
