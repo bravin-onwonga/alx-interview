d353f47d6bc9ab0a0420460959ae1f8ea3d7b77a
bravin-onwonga <onbravin@gmail.com>
2024-08-23 12:51:58 +0300

    Checking for UTF-8 valid

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -9,7 +9,20 @@ def validUTF8(data):
         True - if is valid
         False - not valid
     """
-    for code in data:
-        if code > 128 or code < 1:
-            return False
+    bin_lst = []
+
+    for num in data:
+        bin_lst.append('{0:08b}'.format(num))
+    first_bin = bin_lst[0]
+
+    if first_bin[0] == '0':
+        return True
+    else:
+        idx = 0
+        while first_bin[idx] == '1' and idx < 8:
+            idx += 1
+        while idx < 8:
+            if first_bin[idx] == '1':
+                return False
+            idx += 1
         return True
