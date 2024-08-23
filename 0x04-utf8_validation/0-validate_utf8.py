0541d0af04d6b5bd63f84b6c185b4e83f2d6e070
bravin-onwonga <onbravin@gmail.com>
2024-08-23 16:00:47 +0300

    Checking if number is larger than 3 digits

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -9,20 +9,15 @@ def validUTF8(data):
         True - if is valid
         False - not valid
     """
-    bin_lst = []
 
     for num in data:
-        bin_lst.append('{0:08b}'.format(num))
-    first_bin = bin_lst[0]
-
-    if first_bin[0] == '0':
-        return True
-    else:
-        idx = 0
-        while first_bin[idx] == '1' and idx < 8:
-            idx += 1
-        while idx < 8:
-            if first_bin[idx] == '1':
+        bin = '{0:08b}'.format(num)
+        if bin[0] == '0':
+            continue
+        else:
+            idx = 0
+            while bin[idx] == '1':
+                idx += 1
+            if idx > 3:
                 return False
-            idx += 1
-        return True
+    return True
