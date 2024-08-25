6e3c2e1cbbd506a7471d29b7b1b219eabb234b74
bravin-onwonga <onbravin@gmail.com>
2024-08-25 20:32:06 +0300

    Dealing with only the 8 bits

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """Utf validation"""
 
+from typing import List
 
-def validUTF8(data):
+
+def validUTF8(data: List[int]) -> bool:
     """Checks whether the data passed to the fucntion is
     a valid UTF-8 encoding
     Returns:
@@ -12,35 +14,66 @@ def validUTF8(data):
     bin_lst = []
 
     for num in data:
-        bin_lst.append('{0:08b}'.format(num))
-
-    len_bin_lst = len(bin_lst)
-
-    if (bin_lst[0][0] == '0'):
-        more_than_one = False
-    else:
-        more_than_one = True
-        idx = 0
-        bin = bin_lst[0]
-        while bin[idx] == '1':
-            idx += 1
-        if idx > 4:
-            return False
-        if idx != len_bin_lst:
-            return False
+        bin_lst.append(num_to_bin(num))
 
     idx = 0
-    if more_than_one:
-        idx = 1
+    n = 0
 
-    while idx < len_bin_lst:
-        bin = bin_lst[idx]
+    return check_bin(bin_lst, idx, n)
 
-        if more_than_one:
-            if bin[0] != '1' and bin[2] != 0:
-                return False
+
+def num_to_bin(num: int) -> str:
+    """Converts an interger to its binary rep"""
+    str = ''
+
+    str += '{0:08b}'.format(num)
+
+    len_str = len(str)
+
+    if len_str == 8:
+        return (str)
+    else:
+        str2 = ""
+        count = 0
+        idx = len_str - 1
+        while (count < 8):
+            str2 += str[idx]
+            idx -= 1
+            count += 1
+        return (str2)
+
+
+def check_bin(my_lst: List[str], idx: int, n: int) -> bool:
+    """Recursively checks if the binary number is UTF-8 valid
+    Returns:
+        True - UTF-8 Valid
+        False - Not valid
+    """
+    if (idx == len(my_lst)):
+        return True
+
+    if (n == 0 and my_lst[idx][0] == '0'):
+        return check_bin(my_lst, idx + 1, n)
+
+    if my_lst[idx][0] == '1' and n > 0:
+        num = int(my_lst[idx], 2)
+        if num >> 6 != 2:
+            return False
         else:
-            if bin[0] == '1':
+            if (n):
+                n -= 1
+            return check_bin(my_lst, idx + 1, n)
+
+    if my_lst[idx] == '1' and n == 0:
+        i = 0
+        while [my_lst[idx][i] == '1']:
+            i += 1
+        n = i
+        if n == 1:
+            num = int(my_lst[idx], 2)
+            if num >> 6 != 2:
                 return False
-        idx += 1
-    return True
+            n -= 1
+        return check_bin(my_lst, idx + 1, n)
+
+    return False
