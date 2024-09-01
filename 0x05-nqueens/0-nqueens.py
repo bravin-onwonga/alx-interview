c724ad0ad0f49b607a36388046eb77b966f5f75a
bravin-onwonga <onbravin@gmail.com>
2024-09-02 02:12:29 +0300

    Checking value of n

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -155,5 +155,16 @@ def n_queens(n: int) -> None:
 
 
 if __name__ == '__main__':
-    n = int(sys.argv[1])
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+    try:
+        n = int(sys.argv[1])
+    except ValueError:
+        print("N must be a number")
+        exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        exit(1)
+
     n_queens(n)
