ef4b19ba23adbab0c594becb4f0944e364ead366
bravin-onwonga <onbravin@gmail.com>
2024-09-06 02:21:49 +0300

    In the order they came

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,26 +1,45 @@
 #!/usr/bin/node
-const axios = require('axios');
 
-const film = process.argv;
+const request = require('request');
 
-if (film.length < 3) {
-  console.log('Usage: ./0-starwars_characters.js <film number>');
-  process.exit(1);
-}
+const filmId = process.argv[2];
+const requestURL = 'https://swapi-api.alx-tools.com/api/films';
 
-const url = `https://swapi-api.alx-tools.com/api/films/${film[2]}/`;
-
-axios.get(url).then((res) => {
-  const data = res.data;
-  const charactersUrls = data.characters;
-
-  for (const characterUrl of charactersUrls) {
-    axios.get(characterUrl).then((res) => {
-      console.log(res.data.name);
-    }).catch((error) => {
-      console.log(error);
+const fetchCharacterName = (url) => {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (error) {
+        console.log(error);
+        return;
+      }
+      try {
+        const data = JSON.parse(body);
+        resolve(data.name);
+      } catch (err) {
+        console.log(err);
+      }
     });
+  });
+};
+
+request(requestURL, async (error, response, body) => {
+  if (error) {
+    console.log(error);
+    return;
   }
-}).catch((error) => {
-  console.log(error);
+  body = JSON.parse(body);
+  const filmData = body.results[filmId - 1];
+
+  const characterPromises = filmData.characters.map(characterURL => {
+    let id = characterURL[characterURL.length - 2];
+
+    if (characterURL[characterURL.length - 3] !== '/') {
+      id = `${characterURL[characterURL.length - 3]}${id}`;
+    }
+    const nameURL = `https://swapi-api.alx-tools.com/api/people/${id}/`;
+    return fetchCharacterName(nameURL);
+  });
+
+  const names = await Promise.all(characterPromises);
+  names.forEach((name) => console.log(name));
 });
