0baaf062e38f96c8523045d93ce816c8122d2a3e
bravin-onwonga <onbravin@gmail.com>
2024-09-11 22:43:10 +0300

    chore:Rotate a matrix 90 degrees clockwise

---
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+Simple module to rotate a matrix in place
+Focus on reducing the space complexity involved
+"""
+
+
+def rotate_2d_matrix(matrix):
+    """Rotates a 2d matrix 90 degrees clockwise
+    Param:
+        matrix - size of an n x n matrix
+    Returns:
+        Rotated matrix
+    """
+    n = len(matrix[0])
+    k = 0
+    while (k < n):
+        for x in range(0, int(n / 2)):
+            for y in range(x, n-x-1):
+                tmp = matrix[x][y]
+
+                matrix[x][y] = matrix[y][n-1-x]
+
+                matrix[y][n-1-x] = matrix[n-1-x][n-1-y]
+
+                matrix[n-1-x][n-1-y] = matrix[n-1-y][x]
+
+                matrix[n-1-y][x] = tmp
+        k += 1
+
+    return matrix
